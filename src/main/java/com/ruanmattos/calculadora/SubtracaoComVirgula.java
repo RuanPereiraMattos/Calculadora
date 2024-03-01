0326882aac7526ddcaa69033cce616b52e207d0d
Ruan <ruandupg@gmail.com>
2024-03-01 20:10:29 -0300

    Primeira versão de subtracao com e sem virgula

---
+++ src/main/java/com/ruanmattos/calculadora/SubtracaoComVirgula.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ruanmattos.calculadora;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aluno
+ */
+public class SubtracaoComVirgula {
+
+    public SubtracaoComVirgula() {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Digite o primeiro valor(COM/SEM VIRGULA): ");
+        double valor_um = s.nextDouble();
+        s.nextLine();
+        System.out.print("Digite o segundo valor(COM/SEM VIRGULA): ");
+        double valor_dois = s.nextDouble();
+        s.nextLine();
+        double soma = valor_um - valor_dois;
+        System.out.println("A subtracao do primeiro numero com o segundo numero e: " + soma);
+    }
+
+    public static void main(String[] args) {
+        new SubtracaoComVirgula();
+    }
+    
+}
